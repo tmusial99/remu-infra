e6afe4cbc1beabb86090783cc42444e295a07106
Tobiasz Musiał <tmusial99@gmail.com>
2025-08-03 15:21:11 +0000

    please work this time with more kustomizations

---
+++ apps/astro-bun/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import path from "node:path";
+
+const SITE = process.env.SITE ?? (() => { throw new Error("SITE env not provided."); })();
+const SITE_PATH = path.resolve(`frontend/sites/${SITE}`);
+
+// https://astro.build/config
+export default defineConfig({
+    root: '.',
+    srcDir: SITE_PATH,
+    outDir: `${SITE_PATH}/dist`,
+    vite: {
+        resolve: {
+            alias: {
+                '@shared': path.resolve('./frontend/shared'),
+            }
+        }
+    }
+});
