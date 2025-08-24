45a1c705b7484892ded46642e6b832ec7bc465ad
tmusial99 <tmusial@fandom.com>
2025-08-24 15:19:45 +0200

    Add novi-tech site

--- apps/bun-fiber/frontend/astro.config.mjs
+++ apps/bun-fiber/frontend/astro.config.mjs
@@ -1,23 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import path from "node:path";
+import path from 'node:path';
 
-const SITE = process.env.SITE ?? (() => { throw new Error("SITE env not provided."); })();
+const SITE =
+	process.env.SITE ??
+	(() => {
+		throw new Error('SITE env not provided.');
+	})();
 const SITE_PATH = path.resolve(`src/sites/${SITE}`);
 
 // https://astro.build/config
 export default defineConfig({
-    root: '.',
-    srcDir: SITE_PATH,
-    outDir: `dist/${SITE}`,
-    vite: {
-        resolve: {
-            alias: {
-                '@shared': path.resolve('./src/shared'),
-            }
-        }
-    },
-    server: {
-        allowedHosts: ["remu"],
-    }
+	root: '.',
+	srcDir: SITE_PATH,
+	publicDir: `${SITE_PATH}/public`,
+	outDir: `dist/${SITE}`,
+	vite: {
+		resolve: {
+			alias: {
+				'@shared': path.resolve('./src/shared'),
+			},
+		},
+	},
+	server: {
+		allowedHosts: ['remu'],
+	},
 });
